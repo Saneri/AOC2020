0cc532e0cf6e963fdeeeb1f5e3af97c0342ca0bc
Santeri Suitiala <santeri.suitiala@aalto.fi>
2020-12-06 15:21:52 +0200

    Solve da2 part2

--- day2/solve.js
+++ day2/solve.js
@@ -7,20 +7,19 @@ function solver(input) {
   let correctPassAmount = 0;
   input.forEach((element) => {
     const parts = [...element.matchAll("(\\d+)-(\\d+) (\\w): (\\w+)")][0];
-    const lowBoundary = parseInt(parts[1]);
-    const highBoundary = parseInt(parts[2]);
+    const lowIndex = parseInt(parts[1]) - 1;
+    const highIndex = parseInt(parts[2]) - 1;
     const alphabet = parts[3];
     const pass = parts[4];
 
-    const n = pass.split(alphabet).length - 1;
-    if (lowBoundary <= n && n <= highBoundary) {
+    if ((pass[lowIndex] == alphabet) != (pass[highIndex] == alphabet)) {
       correctPassAmount += 1;
     }
   });
   return correctPassAmount;
 }
 
-assert(solver(["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]) === 2);
+assert(solver(["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]) === 1);
 
 async function main() {
   const input = await utils.readInput("day2/input.txt", utils.types.STRING);
