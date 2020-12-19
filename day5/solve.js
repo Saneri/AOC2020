d613881ff0eef1bd2503706e4b4e750157f7e896
Santeri Suitiala <santeri.suitiala@aalto.fi>
2020-12-19 09:54:56 +0200

    Solve day5 part2

--- day5/solve.js
+++ day5/solve.js
@@ -1,20 +1,25 @@
-const assert = require('assert');
-const _ = require('lodash');
+const assert = require("assert");
 
-const utils = require('../utils');
+const utils = require("../utils");
 
 const rowBlock = 7;
 const columnBlock = 3;
 
 function solver(input) {
-  let maxValue = 0;
+  const seats = [];
   input.forEach((binaryPartition) => {
     const row = binaryPartition.slice(0, rowBlock);
     const column = binaryPartition.slice(rowBlock, rowBlock + columnBlock);
-    const seatId = decode(row, 'F', 'B') * 8 + decode(column, 'L', 'R');
-    if (seatId > maxValue) maxValue = seatId;
+    const seatId = decode(row, "F", "B") * 8 + decode(column, "L", "R");
+    seats.push(seatId);
   });
-  return maxValue;
+  seats.sort((a, b) => a - b);
+  let lastId = seats[0];
+  seats.slice(1).some((id) => {
+    lastId++;
+    return id != lastId;
+  });
+  return lastId;
 }
 
 function decode(str, lowerKey, upperKey) {
@@ -28,21 +33,21 @@ function decode(str, lowerKey, upperKey) {
     } else if (char === upperKey) {
       lower = average;
     } else {
-      throw 'invalid character in str';
+      throw "invalid character in str";
     }
   }
   if (lower != upper - 1) {
-    throw 'Calculation error';
+    throw "Calculation error";
   }
   return lower;
 }
 
-assert(decode('FBFBBFF', 'F', 'B') === 44);
-assert(decode('RLR', 'L', 'R') === 5);
-assert(solver(['FBFBBFFRLR']) === 357);
+assert(decode("FBFBBFF", "F", "B") === 44);
+assert(decode("RLR", "L", "R") === 5);
+//assert(solver(["FBFBBFFRLR"]) === 357);
 
 async function main() {
-  const input = await utils.readInput('day5/input.txt', utils.types.STRING);
+  const input = await utils.readInput("day5/input.txt", utils.types.STRING);
   console.log(solver(input));
 }
 
